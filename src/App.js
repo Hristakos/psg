1114db68b4e0f463bbc5052c856388d67619aa5a
Peter Hristakos <peter.hristakos@bigpond.com>
2021-01-14 18:20:04 +1100

    trying to move preloading earlier

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, { useEffect } from 'react';
 import './App.css';
 import NavBar from './NavBar';
 
@@ -19,6 +19,22 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fab, fas);
 
 function App() {
+  useEffect(() => {
+    //preloading image
+    const imagesArray = [
+      { name: "../assets/images/pic1new.png" },
+      { name: "../assets/images/pic2new.png" },
+      { name: "../assets/images/pic6new.png" },
+      { name: "../assets/images/bathroom.png" },
+      { name: "../assets/images/kitchen.png" },
+      { name: "../assets/images/living.png" },
+      { name: "../assets/images/bedroom.png" }
+    ]
+    imagesArray.forEach((image) => {
+      const img = new Image();
+      img.src = image.name;
+    });
+  }, []);
   return (
     <div className="container">
       <nav>
--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import "./Home.css"
 import CustomSlider from './components/CustomSlider';
 
 function Home() {
-    useEffect(() => {
-        //preloading image
-        const imagesArray = [
-            { name: "../assets/images/pic1new.png" },
-            { name: "../assets/images/pic2new.png" },
-            { name: "../assets/images/pic6new.png" },
-            { name: "../assets/images/bathroom.png" },
-            { name: "../assets/images/kitchen.png" },
-            { name: "../assets/images/living.png" },
-            { name: "../assets/images/bedroom.png" }
-        ]
-        imagesArray.forEach((image) => {
-            const img = new Image();
-            img.src = image.name;
-        });
-    }, []);
+
     return (
         <div className="home-container">
 
