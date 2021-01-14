17cdab01d80c0004edb724d686014e3cc7f062d0
Peter Hristakos <peter.hristakos@bigpond.com>
2021-01-14 19:48:49 +1100

    took out preloading

--- src/App.js
+++ src/App.js
@@ -19,22 +19,32 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fab, fas);
 
 function App() {
-  useEffect(() => {
-    //preloading image
-    const imagesArray = [
-      // { name: "../assets/images/pic1new.png" },
-      // { name: "../assets/images/pic2new.png" },
-      // { name: "../assets/images/pic6new.png" },
-      // { name: "../assets/images/bathroom.png" },
-      // { name: "../assets/images/kitchen.png" },
-      // { name: "../assets/images/living.png" },
-      // { name: "../assets/images/bedroom.png" }
-    ]
-    imagesArray.forEach((image) => {
-      const img = new Image();
-      img.src = image.name;
-    });
-  }, []);
+  // // import im1 from 'img/im1.png'
+  // // import im2 from 'img/im2.png'
+  // // import im3 from 'img/im3.png'
+
+  // // componentDidMount() {
+  // //   imageList = [im1, im2, im3]
+  // //   imageList.forEach((image) => {
+  // //     new Image().src = image
+  // //   });
+  // // }
+  // useEffect(() => {
+  //   //preloading image
+  //   const imagesArray = [
+  //     // { name: "../assets/images/pic1new.png" },
+  //     // { name: "../assets/images/pic2new.png" },
+  //     // { name: "../assets/images/pic6new.png" },
+  //     // { name: "../assets/images/bathroom.png" },
+  //     // { name: "../assets/images/kitchen.png" },
+  //     // { name: "../assets/images/living.png" },
+  //     // { name: "../assets/images/bedroom.png" }
+  //   ]
+  //   imagesArray.forEach((image) => {
+  //     const img = new Image();
+  //     img.src = image.name;
+  //   });
+  // }, []);
   return (
     <div className="container">
       <nav>
--- src/components/CustomSlider.js
+++ src/components/CustomSlider.js
@@ -2,38 +2,8 @@ import React, { Component } from 'react';
 import Slider from 'infinite-react-carousel';
 import "./CustomSlider.css"
 
-import im1 from './assets/images/pic1new.png'
-import im2 from './assets/images/pic2new.png'
-import im3 from './assets/images/pic6new.png'
-import im4 from './assets/images/bathroom.png'
-import im5 from './assets/images/kitchen.png'
-import im6 from './assets/images/living.png'
-import im7 from './assets/images/bedroom.png'
 
-// useEffect(() => {
-//   //preloading image
-//   const imagesArray = [
-//     // { name: "../assets/images/pic1new.png" },
-//     // { name: "../assets/images/pic2new.png" },
-//     // { name: "../assets/images/pic6new.png" },
-//     // { name: "../assets/images/bathroom.png" },
-//     // { name: "../assets/images/kitchen.png" },
-//     // { name: "../assets/images/living.png" },
-//     // { name: "../assets/images/bedroom.png" }
-//   ]
-//   imagesArray.forEach((image) => {
-//     const img = new Image();
-//     img.src = image.name;
-//   });
-// }, []);
 export default class CustomSlider extends Component {
-    componentDidMount() {
-        const imageList = [im1, im2, im3, im4, im5, im6, im7]
-        imageList.forEach((image) => {
-            new Image().src = image
-            console.log("image = " + image)
-        });
-    }
     render() {
         const settings = {
             autoplay: true,
