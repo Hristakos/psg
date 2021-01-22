f5a8f81b9f3fe0cb0a07b536ca21e0f33a53f3af
Peter Hristakos <peter.hristakos@bigpond.com>
2021-01-22 11:31:06 +1100

    changed range box to list items had to rework responsiveness

--- src/OurRange.js
+++ src/OurRange.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./OurRange.css"
 
 import { NavLink, useHistory } from "react-router-dom";
+
 const Range = ({ title, price, image = "./tools.svg", detail, link, id }) => {
     return (
         <div className="range-container">
@@ -12,7 +13,9 @@ const Range = ({ title, price, image = "./tools.svg", detail, link, id }) => {
                 <div className="range-image-container">
                     <img src={image}></img>
                 </div>
-                <div className="range-detail">{detail}</div>
+                <div className="range-detail"><ul>
+                    {detail.map((feature, index) => (<li key={index}>{feature}</li>))}
+                </ul></div>
                 <div className="range-link"><NavLink to={`${link}?id=${id}`} params={{ id: 1 }}><span className="range-link-text"><p>Tell me more</p> <img src="./rangearrow.svg" /></span></NavLink></div>
 
             </div>
@@ -32,7 +35,7 @@ function OurRange() {
                         title="Bronze Model"
                         price="$99,000"
                         image="./permits.png"
-                        detail="1 bedroom / 1 bathroom Open kitchen and living 9 ft ceilings All appliances Fitted to run off electricity grid and sewerage network or septic tank"
+                        detail={["1 bedroom / 1 bathroom", "Open kitchen and living", "9 ft ceilings", "All appliances Fitted to run off electricity grid and sewerage", "network or septic tank"]}
                         link="/RangeDetail"
                         id={1}
                     />
@@ -40,7 +43,7 @@ function OurRange() {
                         title="Silver Model"
                         price="$180,000"
                         image="./noplaster.png"
-                        detail="2 bedrooms / 1 bathroom Open kitchen and living with pitched ceiling All appliances Fitted to run off electricity grid and sewerage network or septic tank Verandah"
+                        detail={["2 bedrooms / 1 bathroom", "Open kitchen and living with pitched ceiling", "All appliances Fitted to run off electricity grid", "sewerage network or septic tank", "Verandah"]}
                         link="/RangeDetail"
                         id={2}
                     />
@@ -50,7 +53,7 @@ function OurRange() {
                         title="Gold Model"
                         price="$199,000"
                         image="./design.png"
-                        detail="3 bedrooms / 1 bathroom Open kitchen and living with pitched ceiling 9ft ceilings All appliances Solar enabled Verandah"
+                        detail={["3 bedrooms / 1 bathroom", "Open kitchen and living with pitched ceiling", "9ft ceilings", "All appliances Solar enabled", "Verandah"]}
                         link="/RangeDetail"
                         id={3}
                     />
@@ -58,7 +61,7 @@ function OurRange() {
                         title="Diamond Model"
                         price="$250,000"
                         image="./design.png"
-                        detail="3 bedroom / 1 bathroom 1 ensuite Open kitchen and living with pitched ceiling 9ft ceilings All appliances Solar enabled Verandah"
+                        detail={["3 bedroom / 1 bathroom", "1 ensuite", "Open kitchen and living with pitched ceiling", "9ft ceilings", "All appliances Solar enabled", "Verandah"]}
                         link="/RangeDetail"
                         id={4}
                     />
