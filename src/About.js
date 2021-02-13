f68d1cfd517271c23ed62bf408056599a79ecc02
Peter Hristakos <peter.hristakos@bigpond.com>
2021-02-13 15:20:32 +1100

    changed spacing of about page

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ function Features() {
                 Modulive is a new housing solution that is flat pack in its design and can be built quickly and at scale.
             </div>
             <div className="about-content">
-                Designed and built by Property Services Group, the Modulive product range combines more than 40 years’ experience in building and construction to deliver a unique and affordable home that can be built and moved anywhere in Australia and beyond.
+                Designed and built by Property Services Group, the Modulive product range combines more than 40 years experience in building and construction to deliver a unique and affordable home that can be built and moved anywhere in Australia and beyond.
             </div>
         </div>
     )
